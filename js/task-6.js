84c3a1bfc18dcaadd33b5c148d98bc82249e9a5c
Dmitriy Dihtiarov <dihtiarovd@gmail.com>
2020-12-02 01:50:52 +0200

    update task 6

--- js/task-6.js
+++ js/task-6.js
@@ -1,14 +1,24 @@
 let total = 0;
 let input = 0;
 
-
-while(true) {
-  input = prompt('Введите число');
+while (true) {
+  let input = prompt('Введите число');
 
   if (input === null) {
-    alert(`Общая сумма чисел равна ${total}`);
-    break;
+    alert('Отменено пользователем');
+    break
+  } 
+
+  input = Number(input);
+
+  if (Number.isNaN(input)) {
+    alert('Введите числовое значение!');
+    continue;
   }
 
-  total += Number(input);
+  total += input;
+}
+
+if (total > 0) {
+  alert(`Общая сумма чисел равна ${total}`);
 }
